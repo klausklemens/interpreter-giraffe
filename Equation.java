6887a15760273625e0b3c447e8de28ebaf299423
Vincent R <v.regener@tu-bs.de>
2019-11-07 19:55:12 +0100

    Added Class-Framework; MathClient.java is the main file

---
+++ Equation.java
@@ -0,0 +1,9 @@
+class Equation {
+	public Equation(String equation) {
+	}
+
+	public int evaluate() {
+		return 0;
+	}
+}
+
---
+++ MathClient.java
@@ -0,0 +1,8 @@
+class MathClient {
+	public static void main(String[] args) {
+		Equation input = new Equation("5 + 2");
+
+		System.out.println(input.evaluate());
+	}
+}
+
