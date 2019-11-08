bf72a6eb8b4eb2fa521d4074f8009d545e2d1561
Vincent R <v.regener@tu-bs.de>
2019-11-09 00:28:19 +0100

    Added new Container Constructor

--- Container.java
+++ Container.java
@@ -1,5 +1,11 @@
 class Container {
+	public boolean isOperator;
 	public String element;
+
+	public Container(String element, boolean isOperator) {
+		this(element);
+		this.isOperator = isOperator;
+	}
 
 	public Container(String element) {
 		this.element = element;
