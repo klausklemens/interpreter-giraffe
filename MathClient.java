7ac67024079ef7e62dd2f437261ca6c18c62aac6
Vincent R <v.regener@tu-bs.de>
2019-11-09 14:54:57 +0100

    Added more Testing functionality

--- MathClient.java
+++ MathClient.java
@@ -2,7 +2,12 @@ class MathClient {
 	public static void main(String[] args) {
 		EquationSolver input = new EquationSolver();
 
-		System.out.println(input.evaluate("7 + 5 * 3 + 2"));
+		System.out.println(input.evaluate("1 + 2 ^ ( 3 + 4 )"));
+		while (true) {
+			String read = System.console().readLine();
+			if (read.equals("exit")) { break; }
+			System.out.println(input.evaluate(read));
+		}
 	}
 }
 
