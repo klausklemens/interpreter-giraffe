9f1ef88cee5e42576f118941e4a162887747355a
Vincent Regener <v.regener@tu-bs.de>
2019-11-28 23:34:40 +0100

    Added testing for NfaRunner class

--- MathClient.java
+++ MathClient.java
@@ -1,12 +1,21 @@
 class MathClient {
 	public static void main(String[] args) {
 		EquationSolver input = new EquationSolver();
+		Transition[] t = {
+				new Transition(0, 1, "a"),
+				new Transition(1, 1, true),
+				new Transition(1, 2, "b"),
+		};
+		NfaRunner stateMachine = new NfaRunner(t, 0, 2);
 
 		System.out.println(input.evaluate("1 + 2 ^ ( 3 + 4 )"));
-		while (true) {
+
+		System.out.println(stateMachine.validate(args[0]));
+		while (args.length > 1) {
 			String read = System.console().readLine();
 			if (read.equals("exit")) { break; }
 			System.out.println(input.evaluate(read));
+			System.out.println(stateMachine.validate(read));
 		}
 	}
 }
