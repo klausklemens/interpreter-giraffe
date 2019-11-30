4310eaefe6ec4e6e31be5e2f0b702c8c4cc21101
Vincent Regener <v.regener@tu-bs.de>
2019-11-30 21:21:40 +0100

    Added support for multiple Final-states

--- Transition.java
+++ Transition.java
@@ -2,39 +2,39 @@
  * State transition
  */
 class Transition {
-    public int from;
-    public int to;
-    public char[] conditions;
-    public boolean alwaysMatch;
+	public int from;
+	public int to;
+	public char[] conditions;
+	public boolean inverted;
 
-    public Transition(int from, int to, String conditions) {
-        this(from, to, conditions.toCharArray());
-    }
+	public Transition(int from, int to, boolean inverted) {
+		this(from, to, (char[]) null, inverted);
+	}
 
-    public Transition(int from, int to, char[] conditions) {
-        this.from = from;
-        this.to = to;
-        this.conditions = conditions;
-        this.alwaysMatch = false;
-    }
+	public Transition(int from, int to, String conditions) {
+		this(from, to, conditions.toCharArray(), false);
+	}
 
-    public Transition(int from, int to, boolean alwaysMatch) {
-        this.from = from;
-        this.to = to;
-        this.alwaysMatch = alwaysMatch;
-    }
+	public Transition(int from, int to, String conditions, boolean inverted) {
+		this(from, to, conditions.toCharArray(), inverted);
+	}
 
-    public boolean matches(char c) {
-        if (this.alwaysMatch) {
-            return true;
-        } else {
-            for (char condition : this.conditions) {
-                if (condition == c) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+	public Transition(int from, int to, char[] conditions, boolean inverted) {
+		this.from = from;
+		this.to = to;
+		this.conditions = conditions;
+		this.inverted = inverted;
+	}
+
+	public boolean matches(char c) {
+		boolean match = false;
+		for (char condition : this.conditions) {
+			if (condition == c) {
+				match = true;
+				break;
+			}
+		}
+		return (match ^ inverted);
+	}
 }
 
